65621b22d66c73643b31079c6fd9971a175927b6
Suleymanov Denis <kingmidas1992@gmail.com>
2019-01-12 03:21:50 +0300

    cors workaround for html template

--- FAQChat/BaseHub.cs
+++ FAQChat/BaseHub.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.IO;
 using Microsoft.AspNet.SignalR;
 
 namespace FAQChat
 {
     public class BaseHub : Hub
     {
+
+        public void LoadChat()
+        {
+            Clients.Caller.sendForm(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"assets\chat_template.htm"));
+        }
+
         public void AskQuestion(string name, string title, string question)
         {
             Clients.All.askQuestion(name, title, question);
--- FAQChat/Scripts/FAQChat.js
+++ FAQChat/Scripts/FAQChat.js
@@ -1,68 +1,93 @@
-﻿$(function () {
-    $.getScript("http://localhost:59913/Scripts/jquery.signalR-2.2.2.min.js", function () {
-        $.getScript("http://localhost:59913/signalr/hubs", function () {
-            $('<link>')
-                .appendTo('head')
-                .attr({
-                    type: 'text/css',
-                    rel: 'stylesheet',
-                    href: 'http://localhost:59913/chat_template.css'
-                });
-            var divElem = $('<div/>')
-                .load('http://localhost:59913/chat_template.html', function () {
-                    $.connection.hub.url = 'http://localhost:59913/signalr';
-                    var chat = $.connection.baseHub;
-                    $.connection.hub.disconnected(function () {
-                        alert("Connection lost");
-                    });
-                    // Create a function that the hub can call to broadcast messages.
-                    chat.client.askQuestion = function (name, title, question) {
-                        // Html encode display name and message.
-                        var encodedName = $('<div />').text(name).html();
-                        var encodedMsg = $('<div />').text(question).html();
-                        // Add the message to the page.
-                        $('#discussion').append('<li><strong>' + encodedName
-                            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
-                    };
+﻿var FAQChat = function (jQuery) {
+    var $ = jQuery;
 
-                    chat.client.addAnswer = function (questionId, text) {
-                        // Html encode display name and message.
-                        var encodedName = $('<div />').text(name).html();
-                        var encodedMsg = $('<div />').text(question).html();
-                        // Add the message to the page.
-                        $('#discussion').append('<li><strong>' + encodedName
-                            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
-                    };
-                    // Get the user name and store it to prepend to messages.
-                    $('#displayname').val(prompt('Enter your name:', ''));
-                    // Set initial focus to message input box.
-                    $('#message').focus();
-                    // Start the connection.
-                    $.connection.hub.start().done(function () {
-                        $('#sendmessage').click(function () {
-                            // Call the Send method on the hub.
-                            console.log($('#displayname').val(), 'test', $('#message').val());
-                            chat.server.askQuestion($('#displayname').val(), 'test', $('#message').val());
-                            // Clear text box and reset focus for next comment.
-                            $('#message').val('').focus();
-                        });
-                    });
-                });
-            divElem.appendTo('body');
-            // Declare a proxy to reference the hub.
-            
+    var _systemObjects = {
+        SERVER_URL: 'http://localhost:59913/',
+        chat: {}
+    };
+
+    var _elements = {};
+
+    var _init = function (initCallback) {
+        $.getScript(_systemObjects.SERVER_URL + "Scripts/jquery.signalR-2.2.2.min.js", function () {
+            $.getScript(_systemObjects.SERVER_URL + "signalr/hubs", initCallback);
         });
-    });
+    };
+
+    var _loadStyles = function () {
+        $('<link>')
+            .appendTo('head')
+            .attr({
+                type: 'text/css',
+                rel: 'stylesheet',
+                href: _systemObjects.SERVER_URL + 'assets/chat_template.css'
+            });
+    };
+
+    var _memorizeElements = function () {
+        _elements['form'] = $('#faq-chat-id--form');
+        _elements['displayname'] = $('#faq-chat-id--displayname');
+        _elements['message'] = $('#faq-chat-id--message');
+        _elements['discussion'] = $('#faq-chat-id--discussion');
+    };
+
+    var _sendMessage = function (e) {
+        e.preventDefault();
+        _systemObjects.chat.server.askQuestion(_elements.displayname.text(), 'test', _elements.message.val());
+        _elements.message.val('').focus();
+    };
+
+    var _recieveMessage = function (name, title, question) {
+        var encodedName = $('<div />').text(name).html();
+        var encodedMsg = $('<div />').text(question).html();
+        _elements.discussion.append('<li><strong>' + encodedName + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+
+
+
+
+    var _startSocket = function () {
+        $.connection.hub.url = _systemObjects.SERVER_URL + '/signalr';
+        $.connection.hub.disconnected(function () {
+
+        });
+
+        
+        _systemObjects.chat = $.connection.baseHub;
+        _systemObjects.chat.client.askQuestion = _recieveMessage;
+        _systemObjects.chat.client.addAnswer = function (questionId, text) { };
+        _systemObjects.chat.client.sendForm = function (content) {
+            var divElem = $('<div />').html(content);
+            divElem.appendTo('body');
+            _memorizeElements();
+
+            _elements.displayname.text(prompt('Enter your name:', ''));
+            _elements.message.focus();
+            _elements.form.submit(_sendMessage);
+        };
+
+        
+        $.connection.hub.start().done(function () {
+            _systemObjects.chat.server.loadChat();            
+        });
+
+
+    };
+
+    return {
+        Init: function () {
+            _init(function () {
+                _loadStyles();
+                _startSocket();
+            });
+        }
+    };
+
+};
+
+
+
+$(function () {
+    var chat = new FAQChat($);
+    chat.Init();
 });
-
-/*var signalRLibrary = document.createElement('script');
-signalRLibrary.type = "text/javascript";
-signalRLibrary.src = 'Scripts/jquery.signalR-2.2.2.min.js';
-document.getElementsByTagName('body')[0].appendChild(signalRLibrary);
-
-var signlaRHub = document.createElement('script');
-signlaRHub.type = "text/javascript";
-signlaRHub.src = "/signalr/hubs";
-document.getElementsByTagName('body')[0].appendChild(signlaRHub);
-*/
-
