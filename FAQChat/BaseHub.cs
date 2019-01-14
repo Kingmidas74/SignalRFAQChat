8d2bb1ed0e266c8f98c7351f27e2c26a51ed7257
Suleymanov Denis <kingmidas1992@gmail.com>
2019-01-14 14:27:20 +0300

    first working beta

--- FAQChat/BaseHub.cs
+++ FAQChat/BaseHub.cs
@@ -8,46 +8,14 @@ namespace FAQChat
 {
     public class BaseHub : Hub
     {
-        private List<Question> _qs = new List<Question>();
         public BaseHub()
         {
-            var questions = new List<Question>();
-            for (var i = 0; i < 10; i++)
-            {
-                var qId = Guid.NewGuid();
-                var q = new Question()
-                {
-                    Id = qId,
-                    Text = "Test" + i.ToString(),
-                    User = new User()
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = "User" + i.ToString()
-                    },
-                    Answers = new List<Answer>()
-                    {
-                        new Answer()
-                        {
-                            Id=Guid.NewGuid(),
-                            QuestionId=qId,
-                            Scores=(new Random().Next()),
-                            Text="Answer+"+i.ToString(),
-                            User = new User()
-                            {
-                                Id=Guid.NewGuid(),
-                                Name="User__"
-                            }
-                        }
-                    }
-                };
-                questions.Add(q);
-            }
-            _qs = questions;
+            
         }
 
         public void LoadChat()
         {
-            Clients.Caller.sendChat(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"assets\chat_template.htm"), _qs);
+            Clients.Caller.sendChat(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"assets\chat_template.htm"), new List<Question>());
         }
 
         public void AskQuestion(Question question)
